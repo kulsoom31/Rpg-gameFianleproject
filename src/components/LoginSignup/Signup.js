266a4d25a24d2d4680347e8bd860cc1acf085add
Deepa Birari <112015631+deepa5053@users.noreply.github.com>
2023-01-19 19:41:56 +0100

    some changes in sign up form

--- src/components/LoginSignup/Signup.js
+++ src/components/LoginSignup/Signup.js
@@ -8,6 +8,7 @@ export const Signup = (props) => {
     const [email, setEmail]= useState('');
     const [pass, setPass]= useState('');
     const [name, setName]= useState('');
+    const [confirm, setConfim]= useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -17,13 +18,13 @@ export const Signup = (props) => {
         <div className="auth-from-container">
             <from className="signup-form" onSubmit={handleSubmit}>
                 <label htmlFor="name">Full name :-
-                <input value={name} name="name" placeholder="full name"/></label>
+                <input value={name} onChange={(e) => setName(e.target.value)} type="name"  placeholder="full name"/></label>
                 <label htmlFor="email">E-mail :-
                 <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="your e-mail"/></label>
                 <label htmlFor="password">Password :-
                 <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="password"/></label>
                 <label htmlFor="password">Confirm Password :-
-                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="Retype password"/></label>
+                <input value={confirm} onChange={(e) => setConfim(e.target.value)} type="password" placeholder="Retype password"/></label>
                 <Link to="/adventure"> 
                 <button type="submit" className="btn-start">Login</button>
                 </Link>
